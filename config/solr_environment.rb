a7b81aa1e4a0a121b6cc018aa4d22c123642f95d
Brian Takita & Bruce Krysiak <pair-brian.takita-and-bruce.krysiak@pivotallabs.com>
2009-05-20 11:31:31 -0700

    solr_environment uses RAILS_ROOT, which allows the solr tasks to be run outside of a Rails project (you can fake RAILS_ROOT).

--- config/solr_environment.rb
+++ config/solr_environment.rb
@@ -2,15 +2,14 @@ ENV['RAILS_ENV']  = (ENV['RAILS_ENV'] || 'development').dup
 # RAILS_ROOT isn't defined yet, so figure it out.
 require "uri"
 dir = File.dirname(__FILE__)
-rails_root_dir = File.expand_path("#{dir}/../../../../")
 SOLR_PATH = File.expand_path("#{dir}/../solr") unless defined? SOLR_PATH
 
-SOLR_LOGS_PATH = "#{rails_root_dir}/log" unless defined? SOLR_LOGS_PATH
-SOLR_PIDS_PATH = "#{rails_root_dir}/tmp/pids" unless defined? SOLR_PIDS_PATH
-SOLR_DATA_PATH = "#{rails_root_dir}/solr/#{ENV['RAILS_ENV']}" unless defined? SOLR_DATA_PATH
+SOLR_LOGS_PATH = "#{RAILS_ROOT}/log" unless defined? SOLR_LOGS_PATH
+SOLR_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids" unless defined? SOLR_PIDS_PATH
+SOLR_DATA_PATH = "#{RAILS_ROOT}/solr/#{ENV['RAILS_ENV']}" unless defined? SOLR_DATA_PATH
 
 unless defined? SOLR_PORT
-  config = YAML::load_file(rails_root_dir+'/config/solr.yml')
+  config = YAML::load_file(RAILS_ROOT+'/config/solr.yml')
   
   SOLR_PORT = ENV['PORT'] || URI.parse(config[ENV['RAILS_ENV']]['url']).port
 end
